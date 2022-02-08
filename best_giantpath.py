1c9b9fdbdf8ab5255a90da50d7d7ae9d774aeaf2
Meep3278 <30444515+Meep3278@users.noreply.github.com>
2022-02-08 17:12:51 -0600

    Fixed negative energy

--- best_giantpath.py
+++ best_giantpath.py
@@ -5,27 +5,28 @@ import random
 from collections import defaultdict
 from simanneal import Annealer
 
+def evaluate_ordering(ordering):
+    eval_list = [1 for _ in range(N)]
+    for i in range(len(ordering)):
+        eval_list[ordering[i][1]] += eval_list[ordering[i][0]]
+    return eval_list
+
 class GiantAnnealer(Annealer):
 
     def move(self):
         # choose a random entry in the matrix
-        i = random.randrange(len(self.state))
-        j = random.randrange(len(self.state))
-        self.state[i], self.state[j] = self.state[j], self.state[i]
+        i = random.randrange(len(self.state)-1)
+        self.state[i], self.state[i+1] = self.state[i+1], self.state[i]
 
     def energy(self):
-        eval_list = [1 for _ in range(N)]
-        for i in range(len(self.state)):
-            eval_list[self.state[i][1]] += eval_list[self.state[i][0]]
-            # print(eval_list)
-        print(sum(eval_list))
-        return sum(eval_list)
+        return -sum(evaluate_ordering(self.state))
 
 if __name__ == '__main__':
-    N = 3
+    N = 4
     init_state = [(a, b) for a in range(N) for b in range(N) if a != b]
     random.shuffle(init_state)
     ga = GiantAnnealer(init_state)
     order, total = ga.anneal()
     print(order)
-    print("total= " + str(total))
+    print(evaluate_ordering(order))
+    print("total=" + str(-total))
